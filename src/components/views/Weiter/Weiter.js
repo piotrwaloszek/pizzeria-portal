9b9f4231f474a9a8cbd5748be667b38b9e81ed07
Piotr Wałoszek <piotrek.waloszek@gmail.com>
2021-06-05 11:43:17 +0200

    Add all views components with styles

---
+++ src/components/views/Weiter/Weiter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Weiter.module.scss';
+
+const Weiter = () => (
+  <div className={styles.component}>
+    <h2>Weiter view</h2>
+  </div>
+);
+
+export default Weiter;
